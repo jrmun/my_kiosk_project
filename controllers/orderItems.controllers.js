b16c537dfa6f4bfed6ae746efac1d54b93905bcb
Lee <jrmun123@gmail.com>
2023-07-26 17:52:59 +0900

    feat: 물건 주문 초기

--- controllers/orderItems.controllers.js
+++ controllers/orderItems.controllers.js
@@ -64,6 +64,7 @@ class OrderItemController {
             return res.status(status).json({ message, result });
         } catch (error) {
             if (error.status) return res.status(error.status).json({ message: error.message });
+            console.log(error);
             return res.status(500).json({ message: '알 수 없는 오류가 발생하였습니다.' });
         }
     };
