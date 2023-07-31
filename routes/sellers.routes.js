79bbe9d7ff3365b1478704a0e273b72829b1b4a5
Lee <jrmun123@gmail.com>
2023-07-31 18:01:58 +0900

    fix: api 경로 수정

--- routes/sellers.routes.js
+++ routes/sellers.routes.js
@@ -5,6 +5,8 @@ const Sellercontroller = require('../controllers/sellers.controllers');
 const sellercontroller = new Sellercontroller();
 
 router.get('/sellers/orderlist', sellercontroller.getOrderList);
+router.get('');
 router.put('/sellers/ordercheck/:order_customer_id', sellercontroller.ordercheck);
+router.put('/sellers/completeorder/:order_customer_id', sellercontroller.completeOrder);
 
 module.exports = router;
